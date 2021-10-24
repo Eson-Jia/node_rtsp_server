4765a6dc33908498516632c4da52abe365231516
jiayuxin <13520723393@163.com>
2021-10-24 23:20:57 +0800

    更新代码,基本上已经完成

--- src/h264.ts
+++ src/h264.ts
@@ -1,13 +1,6 @@
-import { promises } from "fs";
-import {
-    initRTPPacket,
-    RTP_PAYLOAD_TYPE_H264,
-    RTP_VERSION,
-    RTPHeader,
-    RTPPacket,
-    SendRTPPacket,
-    timeoutPromise
-} from "./rtp";
+import {promises} from "fs";
+import {initRTPPacket, RTP_PAYLOAD_TYPE_H264, RTP_VERSION, SendRTPPacket, timeoutPromise} from "./rtp";
+import {createSocket} from "dgram";
 
 
 const MAX_RTP_PAYLOAD = 1400;
@@ -18,6 +11,13 @@ const startCode4 = new Uint8Array([0x00, 0x00, 0x00, 0x01]);
 
 async function main() {
     const fullFile = await promises.readFile('./test.h264');
+    const rtp = createSocket("udp4");
+    rtp.bind(1234, "0.0.0.0");
+    await new Promise((resolve) => {
+        rtp.bind(1234, "0.0.0.0", () => {
+            resolve();
+        });
+    });
     const fps = 25;
     let nextBegin = 0;
     let frameList = new Array<Buffer>();
@@ -39,30 +39,33 @@ async function main() {
             nextBegin = index4 + 4;
         }
     }
-
+    let packet = initRTPPacket(0, 0, 0, RTP_VERSION, RTP_PAYLOAD_TYPE_H264, 0, 0, 0, 0x88923423);
     for (const frame of frameList) {
-        let payload = null;
-        let packet = initRTPPacket(0,0,0,RTP_VERSION,RTP_PAYLOAD_TYPE_H264,0,0,0,0x88923423);
+        const nalType = frame[0];
         if (frame.length > MAX_RTP_PAYLOAD) {//分包
-            let frags = Math.ceil(frame.length / MAX_RTP_PAYLOAD);
-            let fuHeader = 1;
-            for (let index = 0; index < frags; index++) {
-                if (index === 0) {
-                    // FU begin
-                } else if (index === frags - 1) {
-                    // FU end
-                } else {
-                    // FU 中间
+            let leftBuffer = frame.length;
+            let FUIndicator = (nalType & 0x60) | 28;
+            let FUHeader = nalType & 0x1f;
+            while (leftBuffer > 0) {
+                if (leftBuffer == frame.length) {
+                    FUHeader |= 0x80;
+                } else if (leftBuffer < MAX_RTP_PAYLOAD) {
+                    FUHeader |= 0x40;
                 }
-                //填充数据
-                payload = Buffer.concat([Buffer.from([1, 2]), frame.slice(0, MAX_RTP_PAYLOAD)]);
+                let currentSize = leftBuffer > MAX_RTP_PAYLOAD ? MAX_RTP_PAYLOAD : leftBuffer;
+                packet.payload = Buffer.concat([Buffer.from([FUIndicator, FUHeader]), frame.slice(0, currentSize)]);
+                leftBuffer -= currentSize;
+                packet.header.seq++;
             }
         } else {
             //填充数据
-            payload = frame;
+            packet.payload = frame;
         }
-        await SendRTPPacket(packet, 123 as any, 8554, '123123');
-        await timeoutPromise(100);
+        await SendRTPPacket(packet, 123 as any, 8554, '192..168.1.7');
+        if ((nalType & 0x1f) !== 7 && (nalType & 0x1f) !== 8) {
+            packet.header.timestamp += 90000 / fps;
+        }
+        await timeoutPromise(1000 / fps);
     }
 };
 
